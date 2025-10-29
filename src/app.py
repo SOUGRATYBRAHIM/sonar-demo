235621efdab73cefc1cb31d5c19de8e531d3487a
SOUGRATYBRAHIM <brahimsougraty@gmail.com>
2025-10-29 20:37:28 +0100

    Add GitHub Actions workflow for SonarQube

---
+++ src/app.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def greet():
+    name = "Ibrahim"
+    print("Hello!")
+def greet():
+    name = "Ibrahim"
+    print("Hello!")
+def greet():
+    name = "Ibrahim"
+    print("Hello!")
+
